8f8a6f51cb197d3c081bbbeaa9100810ecf5dab0
naustica <nick.haupka@outlook.de>
2021-08-17 19:55:04 +0200

    update config.py

--- api/config.py
+++ api/config.py
@@ -1,12 +1,17 @@
 import os
 
 
+database_uri = os.getenv("DATABASE_URL")
+if database_uri.startswith("postgres://"):
+    database_uri = database_uri.replace("postgres://", "postgresql://", 1)
+
+
 class BaseConfig:
     DEBUG = False
     TESTING = False
     HOST = '0.0.0.0'
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = database_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
